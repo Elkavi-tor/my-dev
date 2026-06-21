9a6c05c9660ebebe20418fcda05975fc2dae8da5
victor kavita <victorkavita5@gmail.com>
2026-06-21 23:00:00 +0300

    added

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 let num = 7;
+if (num > 0) {
+    console.log("The number is positive.");
+} else if (num < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is one.");
+}
+
+
